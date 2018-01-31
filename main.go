e0aed14010abc2bb63aa32fa020dc6e363e7902a
davy <davy.wybiral@gmail.com>
2018-01-30 21:41:33 -0600

    added noscript-update example

--- main.go
+++ main.go
@@ -1,21 +1,43 @@
 package main
 
 import (
-	"github.com/gorilla/mux"
+	"fmt"
+	"log"
 	"net/http"
 	"os"
+	"time"
 )
 
+const fmtstr = `<style>#updates::before{content:"See %d new updates";}</style>`
+
 func main() {
-	r := mux.NewRouter()
-	r.HandleFunc("/", indexHandler).Methods("GET")
-	http.Handle("/", r)
+	http.HandleFunc("/", index)
 	port := os.Getenv("PORT")
 	addr := ":" + port
 	http.ListenAndServe(addr, nil)
 }
 
-func indexHandler(w http.ResponseWriter, r *http.Request) {
+func index(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	w.Write([]byte("Hello world!"))
+	flusher, ok := w.(http.Flusher)
+	if !ok {
+		return
+	}
+	_, err := w.Write([]byte(`<html>
+	<head><title>Noscript updates</title></head>
+	<body><a href="/"><span id="updates"></span></a>`))
+	if err != nil {
+		return
+	}
+	flusher.Flush()
+	updates := 0
+	for {
+		time.Sleep(time.Second)
+		updates += 1
+		_, err := w.Write([]byte(fmt.Sprintf(fmtstr, updates)))
+		if err != nil {
+			return
+		}
+		flusher.Flush()
+	}
 }
